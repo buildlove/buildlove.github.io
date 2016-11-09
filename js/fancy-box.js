234f76ac88427644e57c1fa8b41de9c8bb329a9c
System Administrator <root@liqideMacBook-Pro.local>
2016-11-09 10:29:02 +0800

    Site updated: 2016-11-09 10:29:02

--- js/fancy-box.js
+++ js/fancy-box.js
@@ -1,24 +1,19 @@
 $(document).ready(function() {
-  $('.content img').not('.group-picture img').each(function () {
 
+  $('.content img').each(function () {
     var $image = $(this);
-    var imageTitle = $image.attr('title');
     var $imageWrapLink = $image.parent('a');
 
     if ($imageWrapLink.size() < 1) {
       $imageWrapLink = $image.wrap('<a href="' + this.getAttribute('src') + '"></a>').parent('a');
     }
-
     $imageWrapLink.addClass('fancybox');
-    $imageWrapLink.attr('rel', 'group');
-
-    if (imageTitle) {
-      $imageWrapLink.append('<p class="image-caption">' + imageTitle + '</p>');
-      $imageWrapLink.attr("title", imageTitle); //make sure img title tag will show correctly in fancybox
-    }
+    if ($image.attr("alt")) {
+      $imageWrapLink.append('<div class="pic-title"><span>' + $image.attr("alt") + '</span></div>');
+      $imageWrapLink.attr("title",$image.attr("alt")); //make sure img title tag will show correctly in fancybox
+    };
   });
 });
-
 $('.fancybox').fancybox({
   helpers: {
     overlay: {
